77a9106de54b3b205f35a5531b7834cd05105d23
DimitarMihov <dmihov70@gmail.com>
2014-07-15 20:59:48 +0300

    Task 5

---
+++ CSharpI/ConsoleInputOutput/GreaterOfTwoNumbers/GreaterOfTwoNumbers.cs
@@ -0,0 +1,20 @@
+﻿namespace GreaterOfTwoNumbers
+{
+    //Write a program that gets two numbers from the console and prints the greater of them. Don’t use if statements.
+    
+    using System;
+
+    class GreaterOfTwoNumbers
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter two numbers:");
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            int greaterNumber = Math.Max(firstNumber, secondNumber);
+
+            Console.WriteLine("The greater number is {0}", greaterNumber);
+        }
+    }
+}

7b836da9741453e755c08fba47e380a70e233c6c
DimitarMihov <dmihov70@gmail.com>
2014-07-15 21:11:44 +0300

    Task 7

---
+++ CSharpI/ConsoleInputOutput/GetsNNumbers/GetsNNumbers.cs
@@ -0,0 +1,26 @@
+﻿namespace GetsNNumbers
+{
+    //Write a program that gets a number n and after that gets more n numbers and calculates and prints their sum. 
+    
+    using System;
+
+    class GetsNNumbers
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter number n:");
+            int numberN = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int numberToSum = 0;
+
+            for (int i = 0; i < numberN; i++)
+            {
+                Console.Write("Enter number to sum: ");
+                numberToSum = int.Parse(Console.ReadLine());
+                sum += numberToSum;
+            }
+
+            Console.WriteLine("The sum of numbers is {0}", sum);
+        }
+    }
+}

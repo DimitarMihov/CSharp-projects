d79595bf3a0d5551726a4457eaa6a22192fd85ba
DimitarMihov <dmihov70@gmail.com>
2014-07-15 19:36:44 +0300

    Homework Console Input - Output Tasks 1 and 2

---
+++ CSharpI/ConsoleInputOutput/PerimeterAndAreaOfACircle/PerimeterAndAreaOfACircle.cs
@@ -0,0 +1,20 @@
+﻿namespace PerimeterAndAreaOfACircle
+{
+    //Write a program that reads the radius r of a circle and prints its perimeter and area.
+    
+    using System;
+
+    class PerimeterAndAreaOfACircle
+    {
+        static void Main()
+        {
+            Console.Write("Enter radius: ");
+            double radius = double.Parse(Console.ReadLine());
+
+            double perimeter = 2 * Math.PI * radius;
+            double area = Math.PI * radius * radius;
+
+            Console.WriteLine("Circle with radius {0} has perimeter {1} and area {2}", radius, perimeter, area);
+        }
+    }
+}
---
+++ CSharpI/ConsoleInputOutput/ReadAndPrintNumbers/ReadAndPrintNumbers.cs
@@ -0,0 +1,22 @@
+﻿namespace ReadAndPrintNumbers
+{
+    //Write a program that reads 3 integer numbers from the console and prints their sum.
+    
+    using System;
+
+    class ReadAndPrintNumbers
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter three integer numbers:");
+
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+            int sumOfNumbers = firstNumber + secondNumber + thirdNumber;
+
+            Console.WriteLine(sumOfNumbers);
+
+        }
+    }
+}

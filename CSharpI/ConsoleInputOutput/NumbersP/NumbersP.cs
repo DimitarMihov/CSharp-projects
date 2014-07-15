b57fc41c40f0a29fe59f9f9b497aec0e54f38b68
DimitarMihov <dmihov70@gmail.com>
2014-07-15 20:55:42 +0300

    Task 4

---
+++ CSharpI/ConsoleInputOutput/NumbersP/NumbersP.cs
@@ -0,0 +1,22 @@
+﻿namespace NumbersP
+{
+    //Write a program that reads two positive integer numbers and prints how many numbers p exist between them
+    //such that the reminder of the division by 5 is 0 (inclusive). Example: p(17,25) = 2.
+
+    
+    using System;
+
+    class NumbersP
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter two numbers:");
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            int result = (secondNumber - firstNumber) / 5 + 1;
+
+            Console.WriteLine(result);
+        }
+    }
+}

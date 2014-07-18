b980ea57b1f92839aad6b2b434ea3b229a239eb4
DimitarMihov <dmihov70@gmail.com>
2014-07-18 16:44:34 +0300

    Task 9

---
+++ CSharpI/ConsoleInputOutput/FibonacciSequence/FibonacciSequence.cs
@@ -0,0 +1,37 @@
+﻿namespace FibonacciSequence
+{
+    //Write a program to print the first 100 members of the sequence of Fibonacci:0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, …
+    
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Numerics;
+
+    class FibonacciSequence
+    {
+        static void Main()
+        {
+            Console.WriteLine("0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377,");
+            Console.WriteLine("The first 100 members of the sequence of Fibonacci are:");
+            BigInteger firstNumber = new BigInteger(0);
+            BigInteger secondNumber = new BigInteger(1);
+
+            
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Write("{0}, {1}", firstNumber, secondNumber);
+                firstNumber += secondNumber;
+                secondNumber += firstNumber;
+                if (i != 99)
+                {
+                    Console.Write(", ");
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}

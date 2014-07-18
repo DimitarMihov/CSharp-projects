34d36557fabdfcd294c1215e395f36f04cf9eb73
DimitarMihov <dmihov70@gmail.com>
2014-07-18 18:36:38 +0300

    Task 1

---
+++ CSharpI/Loops/PrintNumbersFromOneToN/PrintNumbersFromOneToN.cs
@@ -0,0 +1,27 @@
+﻿namespace PrintNumbersFromOneToN
+{
+    //Write a program that prints all the numbers from 1 to N.
+    
+    using System;
+
+    class PrintNumbersFromOneToN
+    {
+        static void Main()
+        {
+            Console.Write("Enter a number N as the end of the sequence [1...N]");
+            int numberN = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= numberN; i++)
+            {
+                Console.Write(i);
+
+                if (i != numberN)
+                {
+                    Console.Write(", ");
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}

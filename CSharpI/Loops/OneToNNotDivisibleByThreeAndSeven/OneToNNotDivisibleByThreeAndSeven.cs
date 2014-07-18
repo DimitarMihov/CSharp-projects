5aef6ac4eb80f68d6bb7d58528eedb9401e58eb8
DimitarMihov <dmihov70@gmail.com>
2014-07-18 18:44:28 +0300

    Task 2

---
+++ CSharpI/Loops/OneToNNotDivisibleByThreeAndSeven/OneToNNotDivisibleByThreeAndSeven.cs
@@ -0,0 +1,32 @@
+﻿namespace OneToNNotDivisibleByThreeAndSeven
+{
+    //Write a program that prints all the numbers from 1 to N, that are not divisible by 3 and 7 at the same time.
+    
+    using System;
+
+    class OneToNNotDivisibleByThreeAndSeven
+    {
+        static void Main()
+        {
+            Console.Write("Enter a number: ");
+            int numberN = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= numberN; i++)
+            {
+                if (i % 3 == 0 && i % 7 == 0)
+                {
+                    continue;
+                }
+
+                Console.Write(i);
+
+                if (i != numberN)
+                {
+                    Console.Write(", ");
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}

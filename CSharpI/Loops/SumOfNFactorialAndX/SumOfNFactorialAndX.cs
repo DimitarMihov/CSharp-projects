0f6a2e4a9da0ae26543e5288e47327730a4256d0
DimitarMihov <dmihov70@gmail.com>
2014-07-21 17:22:23 +0300

    Task 6

---
+++ CSharpI/Loops/SumOfNFactorialAndX/SumOfNFactorialAndX.cs
@@ -0,0 +1,32 @@
+﻿namespace SumOfNFactorialAndX
+{
+    //Write a program that, for a given two integer numbers N and X, calculates the sumS = 1 + 1!/X + 2!/X^2 + … + N!/X^N
+    
+    using System;
+
+    class SumOfNFactorialAndX
+    {
+        static void Main()
+        {
+            Console.Write("Enter integer number N: ");
+            double numberN = double.Parse(Console.ReadLine());
+            Console.Write("Enter integer number X: ");
+            double numberX = double.Parse(Console.ReadLine());
+            double sum = 1;
+            
+            for (double i = 1; i <= numberN; i++)
+            {
+                double product = 1;
+
+                for (double j = 1; j <= i; j++)
+                {
+                    product *= j;
+                }
+
+                sum += product / Math.Pow(numberX, i);
+            }
+
+            Console.WriteLine("The result is {0}", sum);
+        }
+    }
+}

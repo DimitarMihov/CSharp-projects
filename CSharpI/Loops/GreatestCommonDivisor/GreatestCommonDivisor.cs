244e0366812b8ac09b32c821e42f9e9500f15abb
DimitarMihov <dmihov70@gmail.com>
2014-07-21 19:23:20 +0300

    Task 8

---
+++ CSharpI/Loops/GreatestCommonDivisor/GreatestCommonDivisor.cs
@@ -0,0 +1,36 @@
+﻿namespace GreatestCommonDivisor
+{
+    //Write a program that calculates the greatest common divisor (GCD) of given two numbers. Use the Euclidean algorithm (find it in Internet).
+    
+    using System;
+
+    class GreatestCommonDivisor
+    {
+        static void Main()
+        {
+            Console.Write("Enter 'a': ");
+            double numberA = double.Parse(Console.ReadLine());
+            Console.Write("Enter 'b': ");
+            double numberB = double.Parse(Console.ReadLine());
+            double gcd;
+
+            if (numberB == 0)
+            {
+                gcd = numberA;
+            }
+            else
+            {
+                while (numberB != 0)
+                {
+                    double aModB = numberA % numberB;
+                    numberA = numberB;
+                    numberB = aModB;
+                }
+
+                gcd = numberA;
+            }
+
+            Console.WriteLine("The greatest common divisor is {0}", gcd);
+        }
+    }
+}

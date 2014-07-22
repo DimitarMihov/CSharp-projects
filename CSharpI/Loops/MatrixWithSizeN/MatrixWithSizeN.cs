3b5cd16f599d12234b241c33248d7da569443253
DimitarMihov <dmihov70@gmail.com>
2014-07-22 19:54:08 +0300

    Task 12

---
+++ CSharpI/Loops/MatrixWithSizeN/MatrixWithSizeN.cs
@@ -0,0 +1,32 @@
+﻿namespace MatrixWithSizeN
+{
+    //Write a program that reads from the console a positive integer number N (N < 20) and outputs a matrix like the following: ...
+    
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class MatrixWithSizeN
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter a positive integer number N < 20 : ");
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++)
+            {
+                int n = i;
+                
+                for (int j = 0; j < number; j++)
+                {
+                    Console.Write(n);
+                    n++;
+                }
+
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}

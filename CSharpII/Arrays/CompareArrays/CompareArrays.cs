93864a573bf10ad7c6b206049591cf2328908d73
DimitarMihov <dmihov70@gmail.com>
2014-07-24 19:41:55 +0300

    Task 2

---
+++ CSharpII/Arrays/CompareArrays/CompareArrays.cs
@@ -0,0 +1,40 @@
+﻿namespace CompareArrays
+{
+    //Write a program that reads two arrays from the console and compares them element by element.
+    
+    using System;
+
+    class CompareArrays
+    {
+        static void Main()
+        {
+            Console.Write("Enter length of the arrays: ");
+            int lengthOfTheArrays = int.Parse(Console.ReadLine());
+            int[] arrayOne = new int[lengthOfTheArrays];
+            int[] arrayTwo = new int[lengthOfTheArrays];
+            bool areSame = true;
+
+            Console.WriteLine("Enter elements of first array:");
+            for (int i = 0; i < lengthOfTheArrays; i++)
+            {
+                arrayOne[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Enter elements of second array:");
+            for (int i = 0; i < lengthOfTheArrays; i++)
+            {
+                arrayTwo[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int j = 0; j < lengthOfTheArrays; j++)
+            {
+                if (arrayOne[j] != arrayTwo[j])
+                {
+                    areSame = false;
+                }
+            }
+
+            Console.WriteLine("Arrays one and two are {0}.", (areSame ? "same" : "not same"));
+        }
+    }
+}

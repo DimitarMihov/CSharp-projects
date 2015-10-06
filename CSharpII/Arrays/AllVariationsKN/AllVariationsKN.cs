feb6a62b78885ed6bb2e08eaf346482350f1e646
DimitarMihov <dmihov70@gmail.com>
2015-10-06 11:05:26 +0300

    Task changed to 20

--- CSharpII/Arrays/AllVariationsKN/AllVariationsKN.cs
+++ CSharpII/Arrays/AllVariationsKN/AllVariationsKN.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 
-//Write a program that reads two numbers N and K and generates all the combinations of K distinct elements from the set [1..N]. Example:
-//    N = 5, K = 2  {1, 2}, {1, 3}, {1, 4}, {1, 5}, {2, 3}, {2, 4}, {2, 5}, {3, 4}, {3, 5}, {4, 5}
+//Write a program that reads two numbers N and K and generates all the variations of K elements from the set [1..N]. Example:
+//    N = 3, K = 2  {1, 1}, {1, 2}, {1, 3}, {2, 1}, {2, 2}, {2, 3}, {3, 1}, {3, 2}, {3, 3}
+
 
 
 class AllVariationsKN

d796b8faef9a3e42746f0c4c8e40a99f51240037
DimitarMihov <dmihov70@gmail.com>
2015-10-06 10:39:20 +0300

    Task 05 initial commit

---
+++ CSharpII/MultidimensionalArrays/SortLengthOfElements/SortLenghtOfElements.cs
@@ -0,0 +1,41 @@
+﻿/*You are given an array of strings. Write a method that sorts the array
+ * by the length of its elements (the number of characters composing them).
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+class SortLenghtOfElements
+{
+    static void Main()
+    {
+        string[] stringArr = { "abcde", "mno", "prqs", "tgones", "ah" };
+
+        SortSize(stringArr);
+    }
+
+    static void SortSize(string[] stringArr)
+    {
+        int n = stringArr.Length;
+        int[] sizes = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            sizes[i] = stringArr[i].Length;
+        }
+
+        Array.Sort(sizes, stringArr);
+
+        foreach (var element in stringArr)
+        {
+            Console.WriteLine(element);
+        }
+
+
+    }
+}
+
+
